0e3d534aa15158496a595a163d0589eb6f8ffc4e
Nelson A Caudill <nelson.a.caudill@gmail.com>
2019-10-12 17:11:09 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,12 +3,15 @@
 # thing is NOT to "complete labs," the important thing is to learn the skills you need
 # in order to complete the rest of this course and be ready for the next course!
 
+# One argument
 def introduction(name)
   puts "Hi, my name is #{name}."
 end
 
-
-
+# Two arguments
+def introduction_with_language(name, language)
+  
+end
 
 
 
